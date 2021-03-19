c66e729c3f84353050f0f16be561adb22553da59
Timur Begishev <rpsi.timurec@gmail.com>
2021-03-20 02:06:30 +0300

    App made

--- TemperatureConverter/ViewController.swift
+++ TemperatureConverter/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var celsiusTemp: UILabel!
+    @IBOutlet weak var fahrenheitTemp: UILabel!
+    @IBOutlet weak var temperatureSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        temperatureSlider.minimumValue = 0
+        temperatureSlider.maximumValue = 150
+        temperatureSlider.value = 0
     }
 
-
+    @IBAction func temperatureChanging(_ sender: UISlider) {
+        celsiusTemp.text = "\(Int(temperatureSlider.value))ºC"
+        fahrenheitTemp.text = "\(Int(temperatureSlider.value * 9 / 5 + 32))ºF"
+    }
+    
 }
-
